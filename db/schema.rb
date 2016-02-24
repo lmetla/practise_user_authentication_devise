ed19b2b7420aa7826220643658747095f651fcfb
lmetla <lmetla@student.unimelb.edu.au>
2016-02-24 12:35:54 +1100

    integrated post_feature with user_authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224010314) do
+ActiveRecord::Schema.define(version: 20160224012121) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
   end
 
   create_table "users", force: :cascade do |t|
